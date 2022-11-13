8e5790f3f95dfd815f2d28e1fea9f58873c03cc8
Muhammad Nauman Raza <thedevraza@proton.me>
2022-11-13 16:47:13 +0000

    chore: remove custom-set-variables

--- init.el
+++ init.el
@@ -46,16 +46,3 @@
 (add-to-list 'load-path (concat user-emacs-directory
         (convert-standard-filename "user/")))
 (load "init")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "d9a28a009cda74d1d53b1fbd050f31af7a1a105aa2d53738e9aa2515908cac4c" "e8567ee21a39c68dbf20e40d29a0f6c1c05681935a41e206f142ab83126153ca" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
