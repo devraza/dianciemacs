9e5ddf2d706caf4c4fa72f439298406e7fe0a1af
Muhammad Nauman Raza <thedevraza@proton.me>
2022-11-16 20:42:23 +0000

    feat: seamless flycheck integration for Rust

--- lisp/languages.el
+++ lisp/languages.el
@@ -4,7 +4,10 @@
 (use-package markdown-mode)
 
 ;; Diagnostics
-(use-package flycheck)
+(use-package flycheck
+  :init
+  (global-flycheck-mode) ; Enable flycheck everywhere by default
+  (push 'rustic-clippy flycheck-checkers)) ; Use clippy for Rust
 
 ;; eglot - Language Server Protocol ingegration
 (use-package eglot)
@@ -17,9 +20,20 @@
   (go-mode . flycheck-mode)) ; Enable flycheck-mode by default
 
 ;; Rust
+(use-package flycheck-rust
+  :init
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)) ; Enable flycheck-rust where flycheck is enabled. If not a rust file, does nothing.
+  
+(use-package flycheck-inline
+  :hook
+  (flycheck-mode . flycheck-inline-mode)) ; Enable flycheck-inline where flycheck is enabled
+
 (use-package rustic
   :custom
   (rustic-lsp-client 'eglot)) ; Make eglot the default LSP client
+
+;; Disable flymake for eglot - in favour of flycheck
+(add-hook 'eglot--managed-mode-hook (lambda () (flymake-mode -1)))
 
 ;; Lua
 (use-package lua-mode)
