8600929eb2fb288778e65ddede1231eec39ec2e2
Muhammad Nauman Raza <thedevraza@proton.me>
2022-11-19 12:19:20 +0000

    feat: better default user init.el

--- user/init.el
+++ user/init.el
@@ -1,1 +1,10 @@
-;; This file contains user configuration. You're completely free to *safely* work with Dianciemacs here.
+;;; init.el --- This file contains user configuration. You're completely free to *safely* work with Dianciemacs here.
+
+;;; Commentary:
+;;
+;; All user configuration should be done from this file,
+;; In order to keep user code clean and manageable
+;;
+;;; Code:
+
+;;; init.el ends here
