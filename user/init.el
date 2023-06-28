ff4c412a3bd508e90189feb8c372a9d4544d7eb3
Muhammad Nauman Raza <thedevraza@proton.me>
2023-06-28 14:56:57 +0100

    chore: org-mode faces improvements

--- user/init.el
+++ user/init.el
@@ -10,4 +10,20 @@
 (add-to-list 'custom-theme-load-path "~/Projects/hazakura-emacs")
 (load-theme 'hazakura t)
 
+(with-eval-after-load 'org
+  ;; Faces for the font sizes of org-mode headings
+  (set-face-attribute 'org-level-8 nil :weight 'bold :family "Urbanist")
+  (set-face-attribute 'org-level-7 nil :weight 'bold :family "Urbanist")
+  (set-face-attribute 'org-level-6 nil :weight 'bold :family "Urbanist")
+  (set-face-attribute 'org-level-5 nil :weight 'bold :family "Urbanist")
+  (set-face-attribute 'org-level-4 nil :weight 'bold :family "Urbanist")
+  (set-face-attribute 'org-level-3 nil :weight 'bold :height 1.05 :family "Urbanist")
+  (set-face-attribute 'org-level-2 nil :weight 'bold :height 1.1 :family "Urbanist")
+  (set-face-attribute 'org-level-1 nil :weight 'bold :height 1.2 :family "Urbanist")
+  (set-face-attribute 'org-document-title nil
+                    :height 1.65
+                    :foreground 'unspecified
+                    :weight 'bold
+		    :family "Urbanist"))
+
 ;;; init.el ends here
