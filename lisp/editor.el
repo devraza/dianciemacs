ec1373dd8d7a14cb103e4ae041df23f1cfe21143
Muhammad Nauman Raza <thedevraza@proton.me>
2023-07-01 18:40:11 +0100

    feat: reintroduce yasnippet

--- lisp/editor.el
+++ lisp/editor.el
@@ -14,13 +14,14 @@
 (global-visual-line-mode 1) ; Prevent wrapping of words
 
 ;; yasnippet - A template system for Emacs
-;; (use-package yasnippet
-;;   :config
-;;   (yas-reload-all)
-;;   (yas-global-mode))
+(use-package yasnippet
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook #'yas-minor-mode)
+  (add-hook 'org-mode-hook #'yas-minor-mode))
 ;; yasnippet-snippets - A snippet collection package by AndreaCotti
-;; (use-package yasnippet-snippets
-;;   :after yasnippet)
+(use-package yasnippet-snippets
+  :after yasnippet)
 
 ;; evil - A Vi Layer inside of Emacs
 (use-package evil
