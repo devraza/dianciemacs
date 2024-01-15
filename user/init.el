8db10757a5f551d832bf16d57d565d6672317504
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-01-15 18:02:54 +0000

    feat: add base init.el

---
+++ user/init.el
@@ -0,0 +1,10 @@
+;;; init.el --- This file contains user configuration. You're completely free to *safely* work with Dianciemacs here.
+
+;;; Commentary:
+;;
+;; All user configuration should be done from this file,
+;; In order to keep user code clean and manageable
+;;
+;;; Code:
+
+;;; init.el ends here
