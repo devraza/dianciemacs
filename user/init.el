852bd54a72aa9ed2149e67dff93202417ea438a4
Muhammad Nauman Raza <devraza@devraza.duckdns.org>
2024-01-15 18:11:32 +0000

    hotfix: add accidentally untracked files

---
+++ user/init.el
@@ -0,0 +1,16 @@
+;;; init.el --- This file contains user configuration. You're completely free to *safely* work with Dianciemacs here.
+
+;;; Commentary:
+;;
+;; All user configuration should be done from this file,
+;; In order to keep user code clean and manageable
+;;
+;;; Code:
+
+;; Set the font
+;; (add-to-list 'default-frame-alist
+;;              '(font . "Iosevka Comfy-11"))
+;; (set-face-attribute 'default nil :family "Iosevka Comfy")
+;; (set-face-attribute 'variable-pitch nil :family "Iosevka Comfy")
+
+;;; init.el ends here
